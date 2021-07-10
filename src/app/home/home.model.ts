6b4089f509b7d6c1c105de919ce8d2aa4d417f10
Steven Leon <stleon2001@gmail.com>
2021-07-10 12:54:54 -0600

    Modelo y Datos ingresados

---
+++ src/app/home/home.model.ts
@@ -0,0 +1,10 @@
+export interface Partido{
+  id: string;
+  equipoCasa: string;
+  equipoVisitante: string;
+  logoEquipoCasa: string;
+  logoEquipoVisitante: string;
+  marcador: {};
+  eventos: string[];
+  estado: string;
+}

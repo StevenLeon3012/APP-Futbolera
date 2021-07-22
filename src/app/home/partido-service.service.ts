66b65b4bf8ecbd7230a55b84aeb08bf062109a15
Steven Leon <stleon2001@gmail.com>
2021-07-21 18:27:36 -0600

    Se corrigieron detalles en vista

--- src/app/home/partido-service.service.ts
+++ src/app/home/partido-service.service.ts
@@ -14,12 +14,12 @@ export class PartidoServiceService {
       marcador: {'equipo1' : 1, 'equipo2': 4},
       logoEquipoCasa: 'assets/Images/Paris_img.png',
       logoEquipoVisitante: 'assets/Images/Real_Madrid_img.png',
-      eventos: ['Inicio el partido',
-                'Goool! de Paris Saint-Germain',
-                'Amarilla! para Neymar',
-                'Goool! de Real Madrid',
-                'Goool! de Real Madrid',
-                'Goool! de Real Madrid'
+      eventos: ['00`: Inicio el partido',
+                '10`: Goool! de Paris Saint-Germain',
+                '25`: Amarilla! para Neymar',
+                '30`: Goool! de Real Madrid',
+                '40`: Goool! de Real Madrid',
+                '80`: Goool! de Real Madrid'
                 ],
       estado: 'En curso'
     },
@@ -30,16 +30,16 @@ export class PartidoServiceService {
       marcador: {'equipo1' : 5, 'equipo2': 4},
       logoEquipoCasa: 'assets/Images/Liga_img.png',
       logoEquipoVisitante: 'assets/Images/Saprissa_img.png',
-      eventos: ['Inicio el partido',
-                'Goool! de Liga Deportiva Alajuelense',
-                'Goool! de Liga Deportiva Alajuelense',
-                'Goool! de Deportivo Saprissa',
-                'Goool! de Liga Deportiva Alajuelense',
-                'Goool! de Deportivo Saprissa',
-                'Goool! de Liga Deportiva Alajuelense',
-                'Goool! de Liga Deportiva Alajuelense',
-                'Goool! de Deportivo Saprissa',
-                'Goool! de Deportivo Saprissa'
+      eventos: ['00`: Inicio el partido',
+                '20`: Goool! de Liga Deportiva Alajuelense',
+                '25`: Goool! de Liga Deportiva Alajuelense',
+                '35`: Goool! de Deportivo Saprissa',
+                '45`: Goool! de Liga Deportiva Alajuelense',
+                '50`: Goool! de Deportivo Saprissa',
+                '57`: Goool! de Liga Deportiva Alajuelense',
+                '60`: Goool! de Liga Deportiva Alajuelense',
+                '85`: Goool! de Deportivo Saprissa',
+                '89`: Goool! de Deportivo Saprissa'
                 ],
       estado: 'En curso'
     },
@@ -50,7 +50,7 @@ export class PartidoServiceService {
       marcador: {'equipo1' : 0, 'equipo2': 0},
       logoEquipoCasa: 'assets/Images/Bayer_img.png',
       logoEquipoVisitante: 'assets/Images/Borussia_Dortmund_img.png',
-      eventos: ['Inicio el partido'],
+      eventos: ['00`: Inicio el partido'],
       estado: 'En curso'
     }
   ];
